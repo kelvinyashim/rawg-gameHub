0a5da931a2c9db6cd7a85f4a6c0134b8b43d8832
Yashim Kelvin Jacob <yashimkelvinjacob@Yashims-MacBook-Pro.local>
2025-08-13 23:05:13 +0100

    implemented dark mode

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,16 @@
-import { HStack, Image, Text } from '@chakra-ui/react'
-import logo from '../assets/download.jpeg'
+import { Button, HStack, Image, Text } from "@chakra-ui/react";
+import logo from "../assets/download.jpeg";
+import { useColorMode } from "@/components/ui/color-mode";
 
 export const NavBar = () => {
+  const { toggleColorMode } = useColorMode();
   return (
     <HStack>
-        <Image src={logo} boxSize='60px'/>
-        <Text>Navbar</Text>
-
+      <Image src={logo} boxSize="60px" />
+      <Text>Navbar</Text>
+      <Button variant="outline" onClick={toggleColorMode}>
+        Toggle Mode
+      </Button>
     </HStack>
-  )
-}
+  );
+};
