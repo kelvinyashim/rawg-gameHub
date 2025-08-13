f2702719c6506723894b61ab3e360c8a7713cc47
Yashim Kelvin Jacob <yashimkelvinjacob@Yashims-MacBook-Pro.local>
2025-08-13 22:32:24 +0100

    added responsive-layout to grids

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import { Button, HStack } from "@chakra-ui/react"
+import { Grid, GridItem,  } from "@chakra-ui/react"
 
 const App = () => {
   return (
-    <HStack>
-      <Button>Click me</Button>
-      <Button>Click me</Button>
-    </HStack>
+    <Grid templateAreas={{
+      base: `"nav" "main"`, //for mobile
+      lg: `"nav nav" "aside main"` 
+    }}>
+      <GridItem area='nav' bg='coral' >Nav</GridItem>
+     
+       <GridItem area='aside' hideBelow='lg' bg='gold' >Aside</GridItem>
+     
+      <GridItem area='main' bg='dodgerblue'>Main</GridItem>
+    </Grid>
   )
 }
 
