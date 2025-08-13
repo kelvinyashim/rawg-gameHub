117d7cbab6705aad894b124f0a8c74670726d05b
Yashim Kelvin Jacob <yashimkelvinjacob@Yashims-MacBook-Pro.local>
2025-08-13 22:51:48 +0100

    added navBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
-import { Grid, GridItem,  } from "@chakra-ui/react"
+import { Grid, GridItem } from "@chakra-ui/react";
+import { NavBar } from "./components/NavBar";
 
 const App = () => {
   return (
-    <Grid templateAreas={{
-      base: `"nav" "main"`, //for mobile
-      lg: `"nav nav" "aside main"` 
-    }}>
-      <GridItem area='nav' bg='coral' >Nav</GridItem>
-     
-       <GridItem area='aside' hideBelow='lg' bg='gold' >Aside</GridItem>
-     
-      <GridItem area='main' bg='dodgerblue'>Main</GridItem>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`, //for mobile
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav">
+        <NavBar/>
+      </GridItem>
+
+      <GridItem area="aside" hideBelow="lg" bg="gold">
+        Aside
+      </GridItem>
+
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
     </Grid>
-  )
-}
+  );
+};
 
 export default App;
